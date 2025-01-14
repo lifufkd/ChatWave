7b20158e955b46d3935fb45af3ea7c2b9c80eec3
SBR <dmsemen45@gmail.com>
2025-01-15 00:39:58 +0300

    Include first router

--- src/main.py
+++ src/main.py
@@ -4,19 +4,20 @@ from fastapi_cache.backends.redis import RedisBackend
 from contextlib import asynccontextmanager
 
 from database import redis_client
-from repository import create_tables
+from repository import create_tables, delete_tables
+from routes import authorization_router
 
 
 @asynccontextmanager
 async def lifespan(_: FastAPI):
     FastAPICache.init(RedisBackend(redis_client), prefix="chatwave-cache")
+    # await delete_tables()  # TODO: Remove after debug
     await create_tables()
     yield
 
 app = FastAPI(lifespan=lifespan)
 
-# app.include_router()
+app.include_router(authorization_router)
 
-# if __name__ == "__main__":
-#     pass
+
 
