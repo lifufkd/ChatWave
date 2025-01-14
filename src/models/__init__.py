14279240a05e494d2ef8790adecd07ef9e1e4899
SBR <dmsemen45@gmail.com>
2025-01-15 00:38:00 +0300

    Change import order

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,5 +1,5 @@
-from .users import Users
 from .blocked_users import BlockedUsers
+from .users import Users
 from .calls import Calls
 from .messages import Messages
 from .unreaded_messages import UnreadMessages
