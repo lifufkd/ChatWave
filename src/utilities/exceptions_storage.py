d740749f73d36363703dcbe8860decb391f169c8
SBR <dmsemen45@gmail.com>
2025-01-16 23:06:47 +0300

    Add new exceptions

--- src/utilities/exceptions_storage.py
+++ src/utilities/exceptions_storage.py
@@ -1,3 +1,12 @@
+from fastapi import status
+from fastapi.exceptions import HTTPException
+
+
+credential_exception = HTTPException(
+    status_code=status.HTTP_401_UNAUTHORIZED,
+    detail="Could not validate credentials",
+    headers={"WWW-Authenticate": "Bearer"}
+)
 
 
 class UserNotFoundError(Exception):
@@ -10,3 +19,21 @@ class InvalidPasswordError(Exception):
 
 class UserAlreadyExists(Exception):
     pass
+
+
+class InvalidFileType(Exception):
+    def __init__(self, detail: str):
+        self.message = detail
+        super().__init__(detail)
+
+
+class FIleToBig(Exception):
+    def __init__(self, detail: str):
+        self.message = detail
+        super().__init__(detail)
+
+
+class ImageCorrupted(Exception):
+    def __init__(self, detail: str):
+        self.message = detail
+        super().__init__(detail)
