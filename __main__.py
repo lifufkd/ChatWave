217eeaf60af904e0a919bdf26719fc71f444917f
SBR <dmsemen45@gmail.com>
2025-01-16 23:01:55 +0300

    Remove auto reload

--- __main__.py
+++ __main__.py
@@ -8,4 +8,4 @@ import uvicorn
 
 # Run the app
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
+    uvicorn.run("main:app", host="127.0.0.1", port=8000)
