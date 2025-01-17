02a76305837fbf003cf76db17e390d7852241119
SBR <dmsemen45@gmail.com>
2025-01-18 00:50:24 +0300

    Add new folder to create

--- src/utilities/file_manager.py
+++ src/utilities/file_manager.py
@@ -16,7 +16,8 @@ class FileManager:
         media_folder = generic_settings.MEDIA_FOLDER
         avatars_folder = media_folder / "avatars"
         messages_folder = media_folder / "messages"
-        for folder in [avatars_folder, messages_folder]:
+        groups_folder = media_folder / "groups"
+        for folder in [avatars_folder, messages_folder, groups_folder]:
             self._create_directory(folder)
 
     @staticmethod
@@ -45,9 +46,8 @@ class FileManager:
         with zipfile.ZipFile(zip_buffer, 'w', zipfile.ZIP_DEFLATED) as zip_file:
             for avatar_path in paths:
                 if self.file_exists(avatar_path):
-                    zip_file.write(avatar_path, arcname=avatar_path.name)  # Добавляем файл в архив
+                    zip_file.write(avatar_path, arcname=avatar_path.name)
 
-        # Перемещаем курсор в начало буфера для чтения
         zip_buffer.seek(0)
 
         return zip_buffer
@@ -58,17 +58,15 @@ class FileManager:
         if file.content_type not in generic_settings.ALLOWED_MEDIA_TYPES:
             raise InvalidFileType(detail=f"Invalid image type. Only {generic_settings.ALLOWED_MEDIA_TYPES} are allowed.")
 
-        # Проверка размера файла
-        file_size_mb = len(file.file.read()) / (1024 * 1024)  # Размер в мегабайтах
-        file.file.seek(0)  # Сбросить указатель чтения для последующей работы
+        file_size_mb = len(file.file.read()) / (1024 * 1024)
+        file.file.seek(0)
         if file_size_mb > generic_settings.MAX_UPLOAD_SIZE:
             raise FIleToBig(detail=f"File size exceeds {generic_settings.MAX_UPLOAD_SIZE} MB limit.")
 
-        # Проверка валидности изображения
         try:
             image = Image.open(file.file)
-            image.verify()  # Проверяет валидность изображения
-            file.file.seek(0)  # Сбросить указатель чтения
+            image.verify()
+            file.file.seek(0)
         except Exception:
             raise ImageCorrupted(detail="Invalid image file.")
 
