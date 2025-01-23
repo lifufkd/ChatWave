aa3f65b9c080c28a3359ea270399239bd5c3c48b
SBR <dmsemen45@gmail.com>
2025-01-23 14:54:08 +0300

    Add endpoint for get user's conversations

--- src/schemas/conversations.py
+++ src/schemas/conversations.py
@@ -1,6 +1,8 @@
+from datetime import datetime
 from pydantic import BaseModel, Field, model_validator, field_validator
+
 from typing import Annotated, Optional
-from utilities import ValidateModelNotEmpty, request_limit
+from utilities import ValidateModelNotEmpty, request_limit, ConversationTypes
 
 
 class UserIds(BaseModel):
@@ -37,3 +39,19 @@ class AddMembersToConversation(BaseModel):
     @field_validator('users_ids', mode='after')
     def set_limits(cls, values):
         return request_limit(values)
+
+
+class GetConversations(BaseModel):
+    id: int
+    creator_id: int
+    type: ConversationTypes
+    name: Annotated[Optional[str], Field(min_length=1, max_length=64)]
+    description: Optional[str]
+    avatar_name: Optional[str]
+    avatar_type: Optional[str]
+    created_at: datetime
+    updated_at: Optional[datetime]
+
+
+class GetConversationsExtended(GetConversations):
+    members_ids: list[int]
