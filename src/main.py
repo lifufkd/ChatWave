0f0984e6e9eb5564f417f9499c5b58dcde10705b
SBR <dmsemen45@gmail.com>
2025-01-25 21:35:34 +0300

    Optimizing imports

--- src/main.py
+++ src/main.py
@@ -1,17 +1,15 @@
 from fastapi import FastAPI
-from fastapi.routing import APIRoute
 from fastapi_cache import FastAPICache
 from fastapi_cache.backends.redis import RedisBackend
 from contextlib import asynccontextmanager
 
 from database import redis_client
-from repository import create_tables, delete_tables
+from repository import create_tables
 from routes import (
     authorization_router,
     users_router,
     conversations_router,
     anonymous_users_router,
-    anonymous_conversations_router,
     messages_router
 )
 from utilities import FileManager
@@ -35,8 +33,6 @@ app.include_router(authorization_router)
 
 app.include_router(anonymous_users_router)
 app.include_router(users_router)
-
-app.include_router(anonymous_conversations_router)
 
 app.include_router(conversations_router)
 
