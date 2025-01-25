58e426436ca52176ea122fbf2bd8e7899baf7b2a
SBR <dmsemen45@gmail.com>
2025-01-25 21:36:35 +0300

    Add new methods

--- src/repository/conversations_members.py
+++ src/repository/conversations_members.py
@@ -1,4 +1,4 @@
-from sqlalchemy import select, delete, and_
+from sqlalchemy import select, delete, and_, func, update, asc
 from models import Conversations, Users, ConversationMembers
 from utilities import ConversationMemberRoles
 from database import session
@@ -32,6 +32,22 @@ async def delete_conversation_members_in_db(conversation_id: int, members_ids: l
         await cursor.commit()
 
 
+async def update_conversation_member_in_db(conversation_id: int, member_id: int, role: ConversationMemberRoles) -> None:
+    async with session() as cursor:
+        query = (
+            update(ConversationMembers)
+            .filter_by(
+                conversation_id=conversation_id,
+                user_id=member_id
+            )
+            .values(
+                role=role
+            )
+        )
+        await cursor.execute(query)
+        await cursor.commit()
+
+
 async def get_conversation_member_role_from_db(user_id: int, conversation_id: int) -> ConversationMemberRoles:
     async with session() as cursor:
         query = (
@@ -43,3 +59,40 @@ async def get_conversation_member_role_from_db(user_id: int, conversation_id: in
         )
         result = await cursor.execute(query)
         return result.scalar()
+
+
+async def get_conversation_members_quantity_in_db(conversation_id: int) -> int:
+    async with session() as cursor:
+        query = (
+            select(func.count())
+            .select_from(ConversationMembers)
+            .filter_by(conversation_id=conversation_id)
+        )
+        result = await cursor.execute(query)
+        return result.scalar()
+
+
+async def get_conversation_members_in_db(conversation_id: int) -> list[ConversationMembers]:
+    async with session() as cursor:
+        query = (
+            select(ConversationMembers)
+            .filter_by(conversation_id=conversation_id)
+            .order_by(asc(ConversationMembers.joined_at))
+        )
+        result = await cursor.execute(query)
+        return result.scalars().all()
+
+
+async def get_conversation_admin_members_from_db(conversation_id: int) -> list[ConversationMembers]:
+    async with session() as cursor:
+        query = (
+            select(ConversationMembers)
+            .filter(
+                and_(
+                    ConversationMembers.conversation_id == conversation_id,
+                    ConversationMembers.role != ConversationMemberRoles.MEMBER
+                )
+            )
+        )
+        result = await cursor.execute(query)
+        return result.scalars().all()
