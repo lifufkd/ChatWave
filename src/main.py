f647c0ea0e26a142c0f54ea185201ee7d74e84e0
SBR <dmsemen45@gmail.com>
2025-01-25 20:06:28 +0300

    Add app name to swagger UI

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.routing import APIRoute
 from fastapi_cache import FastAPICache
 from fastapi_cache.backends.redis import RedisBackend
 from contextlib import asynccontextmanager
@@ -20,19 +21,24 @@ from utilities import FileManager
 async def lifespan(_: FastAPI):
     FileManager().init_folders()
     FastAPICache.init(RedisBackend(redis_client), prefix="chatwave-cache")
-    # await delete_tables()  # TODO: Remove after debug
     await create_tables()
     yield
 
-app = FastAPI(lifespan=lifespan)
+app = FastAPI(
+    title="ChatWave",
+    description="ChatWave - Modern, Simple and Secure REST API for self-hosted messanger",
+    version="1.0.0",
+    lifespan=lifespan
+)
 
 app.include_router(authorization_router)
 
-app.include_router(users_router)
 app.include_router(anonymous_users_router)
+app.include_router(users_router)
 
 app.include_router(anonymous_conversations_router)
 
 app.include_router(conversations_router)
 
 app.include_router(messages_router)
+
