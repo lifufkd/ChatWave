ab849a0a0453e88e99b2787de833ded77a6ade5e
SBR <dmsemen45@gmail.com>
2025-01-26 22:00:24 +0300

    Add arguments type validation for file exceptions

--- src/utilities/exceptions_storage.py
+++ src/utilities/exceptions_storage.py
@@ -1,3 +1,8 @@
+from typing import Union, Literal
+
+from utilities import MessagesTypes
+
+
 class InvalidCredentials(Exception):
     def __init__(self):
         detail = "Could not validate credentials"
@@ -34,20 +39,44 @@ class UserAlreadyExists(Exception):
 
 
 class InvalidFileType(Exception):
-    def __init__(self, detail: str):
-        self.message = detail
+    def __init__(
+            self,
+            file_type_name:
+            Union[
+                Literal[
+                    MessagesTypes.IMAGE,
+                    MessagesTypes.VIDEO,
+                    MessagesTypes.AUDIO,
+                    MessagesTypes.FILE
+                ]
+            ],
+            file_types: str
+    ):
+        detail = f"Invalid {file_type_name} type. Only {file_types} are allowed."
         super().__init__(detail)
 
 
 class FIleToBig(Exception):
-    def __init__(self, detail: str):
-        self.message = detail
+    def __init__(
+            self,
+            file_type_name:
+            Union[
+                Literal[
+                    MessagesTypes.IMAGE,
+                    MessagesTypes.VIDEO,
+                    MessagesTypes.AUDIO,
+                    MessagesTypes.FILE
+                ]
+            ],
+            size_limit: int
+    ):
+        detail = f"{file_type_name} size exceeds {size_limit} MB limit."
         super().__init__(detail)
 
 
 class ImageCorrupted(Exception):
-    def __init__(self, detail: str):
-        self.message = detail
+    def __init__(self):
+        detail = "Image file is corrupted"
         super().__init__(detail)
 
 
