060d9de4e6e36ac6498bfe6e6c19e8e111a6c974
SBR <dmsemen45@gmail.com>
2025-02-06 21:52:19 +0300

    Remove call events methods

--- src/services/unread_messages.py
+++ src/services/unread_messages.py
@@ -1,43 +1,22 @@
-from pathlib import Path
-
 from schemas.unread_messages import AddUnreadMessages, UnreadMessageExistedDTO, AddUnreadMessagesDB
 from validators import (
-    validate_user_in_conversation,
     validate_user_is_message_owner,
-    validate_user_have_access_to_message,
-    validate_user_have_access_to_messages,
-    validate_user_can_manage_messages, validate_users_in_conversation, validate_unread_message_doesnt_exist,
+    validate_users_in_conversation,
+    validate_unread_message_doesnt_exist,
     verify_users_is_existed
 )
 from repository import (
-    insert_text_message,
-    insert_empty_message,
-    insert_media_message,
-    update_message,
-    get_filtered_messages,
-    get_message,
-    get_messages,
-    delete_messages, insert_unread_messages
+    insert_unread_messages
 )
-from schemas import (
-    CreateTextMessageDB,
-    CreateMediaMessage,
-    CreateMediaMessageDB,
-    GetMessage
-)
-from storage import FileManager
-from utilities import (
-    MessagesStatus,
-    MessagesTypes,
-    many_sqlalchemy_to_pydantic,
-    sqlalchemy_to_pydantic,
-    FileNotFound,
-    MediaPatches,
-    SameUsersIds
-)
+from utilities import (SameUsersIds)
 
 
-async def add_unread_messages(user_id: int, conversation_id: int, entity_data: AddUnreadMessages, users_ids: list[int]) -> None:
+async def add_unread_messages(
+        user_id: int,
+        conversation_id: int,
+        entity_data: AddUnreadMessages,
+        users_ids: list[int]
+) -> None:
     if user_id in users_ids:
         raise SameUsersIds()
     if entity_data.message_id is not None:
@@ -64,4 +43,3 @@ async def add_unread_messages(user_id: int, conversation_id: int, entity_data: A
             **entity_data.model_dump(exclude_none=True)
         )
     )
-
