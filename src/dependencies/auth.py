43e57aba03255dc77a15c164cc446ae5bd0bef3e
SBR <dmsemen45@gmail.com>
2025-02-06 14:49:38 +0300

    Fix verify_token_ws method

--- src/dependencies/auth.py
+++ src/dependencies/auth.py
@@ -1,5 +1,5 @@
 from typing import Annotated
-from fastapi import Depends
+from fastapi import Depends, WebSocket
 from jose import JWTError
 
 from utilities import oauth2_scheme, JWT, InvalidCredentials
@@ -13,5 +13,17 @@ async def verify_token(token: Annotated[str, Depends(oauth2_scheme)]) -> int:
             raise InvalidCredentials()
     except JWTError:
         raise InvalidCredentials()
+
+    return user_id
+
+
+async def verify_token_ws(token: str) -> int | None:
+    try:
+        token_payload = JWT.decode_token(token)
+        user_id = token_payload['id']
+        if user_id is None:
+            return None
+    except JWTError:
+        return None
 
     return user_id
