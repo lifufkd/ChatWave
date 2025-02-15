02a8961f1248b9b782aeb8d70df463eefc171869
SBR <dmsemen45@gmail.com>
2025-02-15 23:57:11 +0300

    Add auto generate token method. Add default values for many options

--- src/utilities/config.py
+++ src/utilities/config.py
@@ -1,3 +1,5 @@
+import secrets
+from pydantic import computed_field, Field
 from pydantic_settings import BaseSettings
 from pathlib import Path
 
@@ -5,10 +7,10 @@ from pathlib import Path
 class DBSettings(BaseSettings):
     DB_USER: str
     DB_PASSWORD: str
-    DB_DATABASE: str
-    DB_HOST: str
-    DB_PORT: str
-    DB_SCHEMA: str
+    DB_DATABASE: str = "postgres"
+    DB_HOST: str = "postgres"
+    DB_PORT: int = 5432
+    DB_SCHEMA: str = "chatwave"
 
     @property
     def sqlalchemy_postgresql_url(self):
@@ -26,9 +28,9 @@ class DBSettings(BaseSettings):
 class RedisSettings(BaseSettings):
     REDIS_USER: str | None = None
     REDIS_PASSWORD: str | None = None
-    REDIS_DATABASE: int
-    REDIS_HOST: str
-    REDIS_PORT: str
+    REDIS_DATABASE: int = 0
+    REDIS_HOST: str = "redis"
+    REDIS_PORT: int = 6379
 
     @property
     def redis_url(self):
@@ -48,9 +50,30 @@ class RedisSettings(BaseSettings):
 
 
 class JWTSettings(BaseSettings):
-    JWT_ALGORITHM: str
-    JWT_SECRET_KEY: str
+    JWT_ALGORITHM: str = "HS256"
+    JWT_SECRET_KEY: str = Field(default_factory=lambda: JWTSettings.generate_and_store_secret())
     JWT_ACCESS_TOKEN_EXPIRES: int = 1209600
+
+    @staticmethod
+    def generate_and_store_secret() -> str:
+        env_path = Path(".env")
+        existing_env = {}
+        if env_path.exists():
+            with env_path.open("r", encoding="utf-8") as f:
+                for line in f:
+                    if "=" not in line:
+                        continue
+
+                    key, value = line.strip().split("=", 1)
+                    existing_env[key] = value
+
+        if "JWT_SECRET_KEY" in existing_env:
+            return existing_env["JWT_SECRET_KEY"]
+        new_secret = secrets.token_hex(32)
+        with env_path.open("a", encoding="utf-8") as f:
+            f.write(f"\nJWT_SECRET_KEY={new_secret}\n")
+
+        return new_secret
 
     class Config:
         env_file = ".env"
