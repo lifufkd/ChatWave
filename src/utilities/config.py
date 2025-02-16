81956727eef86bd7dbd0d34f2625a7aff423d06d
SBR <dmsemen45@gmail.com>
2025-02-16 19:52:51 +0300

    Add YamlConfig class

--- src/utilities/config.py
+++ src/utilities/config.py
@@ -1,138 +1,42 @@
-import secrets
-from pydantic import computed_field, Field
-from pydantic_settings import BaseSettings
+import yaml
 from pathlib import Path
 
 
-class DBSettings(BaseSettings):
-    DB_USER: str
-    DB_PASSWORD: str
-    DB_DATABASE: str = "postgres"
-    DB_HOST: str = "postgres"
-    DB_PORT: int = 5432
-    DB_SCHEMA: str = "chatwave"
+class YamlConfig:
+    def __init__(self):
+        self._config_file = Path("config.yml")
 
-    @property
-    def sqlalchemy_postgresql_url(self):
-        return f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASSWORD}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_DATABASE}"
+    def get_value(self, key: str) -> any:
+        data = self._read_config()
+        if key in data:
+            return data[key]
 
-    @property
-    def asyncpg_postgresql_url(self):
-        return f"postgresql://{self.DB_USER}:{self.DB_PASSWORD}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_DATABASE}"
+        return None
 
-    class Config:
-        env_file = ".env"
-        extra = "allow"
+    def add_value(self, key, value):
+        data = self._read_config()
+        if key not in data:
+            data[key] = value
+        self._write_config(data)
 
+    def update_value(self, key: str, value) -> None:
+        data = self._read_config()
+        if key in data:
+            data[key] = value
+        self._write_config(data)
 
-class RedisSettings(BaseSettings):
-    REDIS_USER: str | None = None
-    REDIS_PASSWORD: str | None = None
-    REDIS_DATABASE: int = 0
-    REDIS_HOST: str = "redis"
-    REDIS_PORT: int = 6379
+    def delete_value(self, key: str) -> None:
+        data = self._read_config()
+        if key in data:
+            del data[key]
+        self._write_config(data)
 
-    @property
-    def redis_url(self):
-        if self.REDIS_USER:
-            redis_user = self.REDIS_USER
-        else:
-            redis_user = ""
-        if self.REDIS_PASSWORD:
-            redis_password = self.REDIS_PASSWORD
-        else:
-            redis_password = ""
-        return f"redis://{redis_user}:{redis_password}@{self.REDIS_HOST}:{self.REDIS_PORT}/{self.REDIS_DATABASE}"
+    def _write_config(self, data: dict) -> None:
+        with open(self._config_file, "w") as file:
+            yaml.dump(data, file)
 
-    class Config:
-        env_file = ".env"
-        extra = "allow"
-
-
-class JWTSettings(BaseSettings):
-    JWT_ALGORITHM: str = "HS256"
-    JWT_SECRET_KEY: str = Field(default_factory=lambda: JWTSettings.generate_and_store_secret())
-    JWT_ACCESS_TOKEN_EXPIRES: int = 1209600
-
-    @staticmethod
-    def generate_and_store_secret() -> str:
-        env_path = Path(".env")
-        existing_env = {}
-        if env_path.exists():
-            with env_path.open("r", encoding="utf-8") as f:
-                for line in f:
-                    if "=" not in line:
-                        continue
-
-                    key, value = line.strip().split("=", 1)
-                    existing_env[key] = value
-
-        if "JWT_SECRET_KEY" in existing_env:
-            return existing_env["JWT_SECRET_KEY"]
-        new_secret = secrets.token_hex(32)
-        with env_path.open("a", encoding="utf-8") as f:
-            f.write(f"\nJWT_SECRET_KEY={new_secret}\n")
-
-        return new_secret
-
-    class Config:
-        env_file = ".env"
-        extra = "allow"
-
-
-class GenericSettings(BaseSettings):
-    MEDIA_FOLDER: Path
-    ALLOWED_IMAGE_TYPES: list[str] = [
-        "image/jpeg",
-        "image/png",
-        "image/gif",
-        "image/webp",
-        "image/svg+xml",
-        "image/bmp",
-        "image/tiff",
-        "image/x-icon"
-    ]
-
-    ALLOWED_VIDEO_TYPES: list[str] = [
-        "video/mp4",
-        "video/webm",
-        "video/ogg",
-        "video/quicktime",
-        "video/x-msvideo",
-        "video/x-flv",
-        "video/x-matroska",
-        "video/mpeg",
-        "video/3gpp",
-        "video/3gpp2"
-    ]
-
-    ALLOWED_AUDIO_TYPES: list[str] = [
-        "audio/mpeg",
-        "audio/wav",
-        "audio/ogg",
-        "audio/webm",
-        "audio/aac",
-        "audio/flac",
-        "audio/x-wav",
-        "audio/x-m4a",
-        "audio/x-flac",
-        "audio/mp4",
-        "audio/midi",
-        "audio/x-midi"
-    ]
-    MAX_UPLOAD_IMAGE_SIZE: int = 30
-    MAX_UPLOAD_VIDEO_SIZE: int = 8192
-    MAX_UPLOAD_AUDIO_SIZE: int = 512
-    MAX_UPLOAD_FILE_SIZE: int = 16384
-    CHUNK_SIZE: int = 16
-    MAX_ITEMS_PER_REQUEST: int = 100
-
-    class Config:
-        env_file = ".env"
-        extra = "allow"
-
-
-redis_settings = RedisSettings()
-db_settings = DBSettings()
-jwt_settings = JWTSettings()
-generic_settings = GenericSettings()
+    def _read_config(self) -> dict:
+        if not self._config_file.is_file():
+            self._write_config(data={})
+        with open(self._config_file, "r") as file:
+            return yaml.safe_load(file)
