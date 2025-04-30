747563d58f1edbfa8a3bc9b2f820b15b0e450f78
SBR <dmsemen45@gmail.com>
2025-04-30 22:05:38 +0300

    Fix pg_max_length when try to process long content row

--- src/triggers/handlers.py
+++ src/triggers/handlers.py
@@ -5,9 +5,8 @@ from storage import FileManager
 from utilities import MediaPatches
 
 
-async def handle_unread_messages_changes(payload: str):
-    row_data = json.loads(payload)
-    await redis_client.publish("user:unread_messages_events", str(row_data.get("user_id")))
+async def handle_unread_messages_changes(user_id: str):
+    await redis_client.publish("user:unread_messages_events", user_id)
 
 
 async def handle_recipients_change(payload: str):
@@ -21,12 +20,11 @@ async def handle_recipients_change(payload: str):
     await redis_client.publish("user:recipients_change_events", data)
 
 
-async def handle_user_delete_changes(payload: str):
-    row_data = json.loads(payload)
+async def handle_user_delete_changes(avatar_name: str):
     file_manager = FileManager()
-    filepath = MediaPatches.USERS_AVATARS_FOLDER.value / f"{row_data.get('avatar_name')}"
+    filepath = MediaPatches.USERS_AVATARS_FOLDER.value / avatar_name
 
-    if row_data.get('avatar_name') is None:
+    if avatar_name is None:
         return None
     if not (await file_manager.file_exists(file_path=filepath)):
         return None
@@ -34,12 +32,11 @@ async def handle_user_delete_changes(payload: str):
     await file_manager.delete_file(file_path=filepath)
 
 
-async def handle_conversation_delete_changes(payload: str):
-    row_data = json.loads(payload)
+async def handle_conversation_delete_changes(avatar_name: str):
     file_manager = FileManager()
-    filepath = MediaPatches.GROUPS_AVATARS_FOLDER.value / f"{row_data.get('avatar_name')}"
+    filepath = MediaPatches.GROUPS_AVATARS_FOLDER.value / avatar_name
 
-    if row_data.get('avatar_name') is None:
+    if avatar_name is None:
         return None
     if not (await file_manager.file_exists(file_path=filepath)):
         return None
@@ -47,12 +44,11 @@ async def handle_conversation_delete_changes(payload: str):
     await file_manager.delete_file(file_path=filepath)
 
 
-async def handle_messages_delete_changes(payload: str):
-    row_data = json.loads(payload)
+async def handle_messages_delete_changes(file_content_name: str):
     file_manager = FileManager()
-    filepath = MediaPatches.MEDIA_MESSAGES_FOLDER.value / f"{row_data.get('file_content_name')}"
+    filepath = MediaPatches.MEDIA_MESSAGES_FOLDER.value / file_content_name
 
-    if row_data.get('file_content_name') is None:
+    if file_content_name is None:
         return None
     if not (await file_manager.file_exists(file_path=filepath)):
         return None
