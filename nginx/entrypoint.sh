c33039ba34f4affb779eaf6f7430162b1357a33f
SBR <dmsemen45@gmail.com>
2025-05-04 23:14:02 +0300

    Change SSL mod from traefic to nginx

--- nginx/entrypoint.sh
+++ nginx/entrypoint.sh
@@ -1,32 +1,8 @@
 #!/bin/sh
 set -e
 
-# Определяем включён ли SSL
-if [ -n "$SSL_CERT_PATH" ] && [ -n "$SSL_CERT_KEY" ]; then
-  export SSL_REDIRECT_OR_PROXY="return 301 https://\$host\$request_uri;"
-
-  export SSL_SERVER_BLOCK="server {
-    listen 443 ssl;
-    server_name localhost;
-
-    ssl_certificate     $SSL_CERT_PATH;
-    ssl_certificate_key $SSL_CERT_KEY;
-
-    location / {
-        proxy_pass http://chatwave:8000;
-        proxy_set_header Host \$host;
-        proxy_set_header X-Real-IP \$remote_addr;
-    }
-  }"
-else
-  export SSL_REDIRECT_OR_PROXY="location / {
-    proxy_pass http://chatwave:8000;
-    proxy_set_header Host \$host;
-    proxy_set_header X-Real-IP \$remote_addr;
-  }"
-
-  export SSL_SERVER_BLOCK=""
-fi
+export SSL_CERT_PATH=$SSL_CERT_PATH
+export SSL_CERT_KEY=$SSL_CERT_KEY
 
 # Подставляем переменные в шаблон
 envsubst "$(env | cut -d= -f1 | sed 's/^/$/')" < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
